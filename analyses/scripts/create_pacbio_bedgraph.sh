26b8696b8573891f64cf316b3317c9b27afbb42a
Rohit Kolora <rohit.kolora@nationwidechildrens.org>
2025-08-28 23:06:55 +0000

    pacbio metilene comments

--- analyses/scripts/create_pacbio_bedgraph.sh
+++ analyses/scripts/create_pacbio_bedgraph.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+### Creating Bedgraph files for Metilene output
+
 set -e
 
 mkdir -p outputs/filt_bedgraph && cd outputs/filt_bedgraph ;
@@ -9,7 +11,7 @@ find /mnt/project/analysis/ -type f -name "*bed*" | fgrep -v -e '.tbi' -e 'singl
   echo "Processing - ${outfile1}"
   if [[ ! -f "${outfile1}" ]]; then
     zcat ${infile1} | tail -n +9 | awk -F'\t' '$6>=5' | awk -F'\t' 'BEGIN{OFS="\t"} {print $1,$2,$3,$4}' >tmp.${outfile1} ; # Atleast 5X coverage and get coords
-    sortBed -i tmp.${outfile1} > ${outfile1} ; # Sorting the bedfile
+    sortBed -i tmp.${outfile1} > ${outfile1} ; # Sorting the bedfile 
     rm -f tmp.${outfile1} ;
   fi  
 done  
