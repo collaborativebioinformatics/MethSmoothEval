6bb5e9b382b91fb5b8d188f3823fb36bc0418fe3
Rohit Kolora <rohit.kolora@nationwidechildrens.org>
2025-08-28 17:06:02 +0000

    modified pacbio script to avoid redundancy

--- analyses/scripts/run_pbcpgtools.sh
+++ analyses/scripts/run_pbcpgtools.sh
@@ -7,6 +7,8 @@ mkdir -p outputs && cd outputs ;
 find ../datasets/ -type f -name "*.bam.bai" | fgrep '.pacbio.' | while read bamindex1; do
   filename1=$(basename "${bamindex1}") ;
   echo "$filename1" ;
-  aligned_bam_to_cpg_scores --bam ${bamindex1/.bai/} --output-prefix ${filename1/.bam.bai/} --threads 16
+  if [[ ! -f "${filename1/.bam.bai/.combined.bed.gz.tbi}" ]]; then
+    aligned_bam_to_cpg_scores --bam ${bamindex1/.bai/} --output-prefix ${filename1/.bam.bai/} --threads 16
+  fi  
 done
 
